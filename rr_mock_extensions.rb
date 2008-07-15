084afd4f0b8e7783b3ce20162545470459dbe32b
Joseph Wilk <josephwilk@joesniff.co.uk>
2008-07-15 12:30:58 +0100

    mock_helper for rr

---
+++ rr_mock_extensions.rb
@@ -0,0 +1,50 @@
+module Spec::Rails::Mocks
+
+  # Creates a mock object instance for a +model_class+ with common
+  # methods stubbed out. Additional methods may be easily stubbed (via
+  # add_stubs) if +stubs+ is passed.
+  def mock_model(model_class, options_and_stubs = {})
+
+    m = eval "#{model_class}.new"
+
+    id = next_id
+    options_and_stubs.reverse_merge!({
+      :id => id,
+      :to_param => "#{id}",
+      :new_record? => false
+      # :errors => ''# stub("errors", :count => 0) - cannot do this in RR
+    })
+    
+    #Errors needs a proxy
+    if options_and_stubs.has_key?(:errors)
+      stub(m.errors).count{options_and_stubs[:errors]}
+    else
+      stub(m.errors).count{0}
+    end
+
+    options_and_stubs.each do |method,value|
+      value = "'#{value}'" if value.instance_of? String
+      eval "stub(m).#{method}{#{value}}"
+    end
+
+    # m = mock("#{model_class.name}_#{options_and_stubs[:id]}", options_and_stubs)
+    #
+    # m.send(:__mock_proxy).instance_eval <<-CODE
+    #   def @target.is_a?(other)
+    #     #{model_class}.ancestors.include?(other)
+    #   end
+    #   def @target.kind_of?(other)
+    #     #{model_class}.ancestors.include?(other)
+    #   end
+    #   def @target.instance_of?(other)
+    #     other == #{model_class}
+    #   end
+    #   def @target.class
+    #     #{model_class}
+    #   end
+    # CODE
+    yield m if block_given?
+    m
+  end
+  
+end
